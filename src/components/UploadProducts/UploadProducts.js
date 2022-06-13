a98757a98ad860cf0cf1528135f9fa7bfceeb799
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2022-06-13 11:44:14 +0700

    All frontend and dark part done

--- src/components/UploadProducts/UploadProducts.js
+++ src/components/UploadProducts/UploadProducts.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const UploadProducts = () => {
   return (
     <div>
-      <h1>Upload Here</h1>
+      <h2 className="text-center">Upload Product</h2>
+      <div className="w-50 mx-auto mt-5 form p-5">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Product Price</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="danger" type="submit">
+            Upload
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
